401364bcfe6dfee8cfab3bb7d64a7ffd0e193ba8
Michael Munn <munn@google.com>
2020-11-02 02:31:22 +0000

    cleaned up toy_train, added more to README

--- README.md
+++ README.md
@@ -1,18 +1,65 @@
 # COT-GAN: Generating Sequential Data via Causal Optimal Transport
-This repository contains an implementation and further details of COT-GAN: Generating Sequential Data via Causal Optimal Transport submitted to 34th Conference on Neural Information Processing Systems (NeurIPS 2020).
+Authors: Tianlin Xu, Li K. Wenliang, Michael Munn, Beatrice Acciaio
 
-## Examples of generated videos
+COT-GAN is an adversarial algorithm to train implicit generative models optimized for producing sequential data. The loss function of this algorithm is formulated using ideas from Causal Optimal Transport (COT), which combines classic optimal transport methods with an additional temporal causality constraint. 
 
-### Sprites
+This repository contains an implementation and further details of COT-GAN. 
+
+Reference: Tianlin Xu, Li K. Wenliang, Michael Munn, Beatrice Acciaio, "COT-GAN: Generating Sequential Data via Causal Optimal Transport," Neural Information Processing Systems (NeurIPS), 2020.
+
+Paper Link: https://arxiv.org/abs/2006.08571 (replace with Neurips link)
+
+Contact: tianlin.xu1@gmail.com
+
+## Setup
+
+Begin by installing pip and setting up virtualenv.
+
+```
+$ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+$ python get-pip.py
+$ python3 -m pip install virtualenv
+```
+
+### Clone the Github repository and install requirements
+
+```
+$ git clone https://github.com/tianlinxu312/cot-gan.git
+$ cd cot-gan/
+
+# Create a virtual environment called 'venv'
+$ virtualenv venv 
+$ source venv/bin/activate    # Activate virtual environment
+$ pip install -r requirements.txt 
+```
+## Training COT-GAN
+We trained COT-GAN on synthetic low-dimensional datasets as well as two high-dimensional video datasets: a [human action dataset](http://www.wisdom.weizmann.ac.il/~vision/SpaceTimeActions.html) and an [animated Sprites dataset](https://github.com/jrconway3/Universal-LPC-spritesheet)
+
+For training on low-dimensional datasets, use a flag to specify either synthetic time series sine data (`SineImage`), auto-regressive data of order one (`AROne`), or EEG data (`eeg`). For example, to train on AR-1 data:
+```
+python3 -m toy_train \
+  --dname="AROne"
+```
+See the code for how to modify the default values of other training parameters or hyperparameters.
+
+Similarly, for training on video datasets, specify either the human action or animated Sprites dataset; either `human_action` or `animated`, resp.
+
+```
+python3 -m video_train \
+  --dname="human_action"
+```
+
+### Data
+.tfrecord files used in COT-GAN experiments can be downloaded here: https://drive.google.com/drive/folders/1ja9OlAyObPTDIp8bNl8rDT1RgpEt0qO-?usp=sharing
+
+## Results
+
+### Animated Sprites
 
 <img src="./figs/animation.gif" width="360" height="120"/>
 
-### Human actions 
+### Human Actions 
 
 <img src="./figs/humanaction.gif" width="360" height="120"/>
 
-## Dependencies
-Python 3.5 and above and Tensorflow 2.x
 
-## Data
-.tfrecord files used in COT-GAN experiments can be downloaded here: https://drive.google.com/drive/folders/1ja9OlAyObPTDIp8bNl8rDT1RgpEt0qO-?usp=sharing
--- toy_train.py
+++ toy_train.py
@@ -1,55 +1,36 @@
-import matplotlib as mpl
-
-mpl.use("Agg")
+import argparse
+import data_utils
+import gan_utils
+import gan
 import os
+import math
+import time
+import tqdm
+
+import numpy as np
+import tensorflow as tf
+import tensorflow_probability as tfp
+
+from datetime import datetime
+from functools import partial
+
+import matplotlib as mpl
+mpl.use("Agg")
+
 os.environ["OMP_NUM_THREADS"] = "4"
 os.environ["OPENBLAS_NUM_THREADS"] = "4"
 os.environ["MKL_NUM_THREADS"] = "4"
 os.environ["VECLIB_MAXIMUM_THREADS"] = "4"
 os.environ["NUMEXPR_NUM_THREADS"] = "4"
-import numpy as np
-import tensorflow as tf
-import tensorflow_probability as tfp
-import data_utils
-import time
-import gan_utils
-import gan
-import math
-from datetime import datetime
-import argparse, tqdm
-from functools import partial
-
-start_time = time.time()
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 tf.keras.backend.set_floatx('float32')
 
-parser = argparse.ArgumentParser(description='cot')
-parser.add_argument('-d', '--dname', type=str, default='SineImage', choices=['SineImage', 'AROne', 'eeg'])
-parser.add_argument('-t', '--test', type=str, default='cot', choices=['cot'])
-parser.add_argument('-s', '--seed', type=int, default=42)
-parser.add_argument('-gss', '--g_state_size', type=int, default=32)
-parser.add_argument('-dss', '--d_state_size', type=int, default=32)
-parser.add_argument('-gfs', '--g_filter_size', type=int, default=32)
-parser.add_argument('-dfs', '--d_filter_size', type=int, default=32)
-parser.add_argument('-r', '--reg_penalty', type=float, default=10.0)
-parser.add_argument('-ts', '--time_steps', type=int, default=48)
-parser.add_argument('-sinke', '--sinkhorn_eps', type=float, default=100)
-parser.add_argument('-sinkl', '--sinkhorn_l', type=int, default=100)
-parser.add_argument('-Dy', '--Dy', type=int, default=10)
-parser.add_argument('-Dz', '--z_dims_t', type=int, default=10)
-parser.add_argument('-g', '--gen', type=str, default="lstm", choices=["lstm", "fc"])
-parser.add_argument('-bs', '--batch_size', type=int, default=32)
-parser.add_argument('-nlstm', '--nlstm', type=int, default=1, help="number of lstms in discriminator")
-parser.add_argument('-lr', '--lr', type=float, default=1e-3)
-parser.add_argument('-bn', '--bn', type=int, default=1, help="batch norm")
-
-args = parser.parse_args()
+start_time = time.time()
 
 
-def main():
-    #
+def train(args):
     # hyper-parameter settings
-    #
     dname = args.dname
     test = args.test
     time_steps = args.time_steps
@@ -107,16 +88,21 @@ def main():
     disc_kernel_width = 5
 
     if args.gen == "fc":
-        generator = gan.SimpleGenerator(batch_size, time_steps, Dx, g_filter_size, z_dims_t,
-                                        output_activation=g_output_activation)
+        generator = gan.SimpleGenerator(
+            batch_size, time_steps, Dx, g_filter_size, z_dims_t,
+            output_activation=g_output_activation)
     elif args.gen == "lstm":
-        generator = gan.ToyGenerator(batch_size, time_steps, z_dims_t, Dx, g_state_size, g_filter_size,
-                                     output_activation=g_output_activation, nlstm=nlstm, nlayer=2, Dy=y_dims, bn=bn)
+        generator = gan.ToyGenerator(
+            batch_size, time_steps, z_dims_t, Dx, g_state_size, g_filter_size,
+            output_activation=g_output_activation, nlstm=nlstm, nlayer=2,
+            Dy=y_dims, bn=bn)
 
-    discriminator_h = gan.ToyDiscriminator(batch_size, time_steps, z_dims_t, Dx, d_state_size, d_filter_size,
-                                           kernel_size=disc_kernel_width, nlayer=2, nlstm=0, bn=bn)
-    discriminator_m = gan.ToyDiscriminator(batch_size, time_steps, z_dims_t, Dx, d_state_size, d_filter_size,
-                                           kernel_size=disc_kernel_width, nlayer=2, nlstm=0, bn=bn)
+    discriminator_h = gan.ToyDiscriminator(
+        batch_size, time_steps, z_dims_t, Dx, d_state_size, d_filter_size,
+        kernel_size=disc_kernel_width, nlayer=2, nlstm=0, bn=bn)
+    discriminator_m = gan.ToyDiscriminator(
+        batch_size, time_steps, z_dims_t, Dx, d_state_size, d_filter_size,
+        kernel_size=disc_kernel_width, nlayer=2, nlstm=0, bn=bn)
 
     # data_utils.check_model_summary(batch_size, z_dims, generator)
     # data_utils.check_model_summary(batch_size, seq_len, discriminator_h)
@@ -124,14 +110,15 @@ def main():
     lsinke = int(np.round(np.log10(sinkhorn_eps)))
     lreg = int(np.round(np.log10(reg_penalty)))
     saved_file = f"{dname[:3]}_{test[0]}_e{lsinke:d}r{lreg:d}s{seed:d}" + \
-                 "{}_{}{}-{}:{}:{}.{}".format(dataset, datetime.now().strftime("%h"),datetime.now().strftime("%d"),
-                                              datetime.now().strftime("%H"), datetime.now().strftime("%M"),
-                                              datetime.now().strftime("%S"), datetime.now().strftime("%f"))
+        "{}_{}{}-{}:{}:{}.{}".format(dataset, datetime.now().strftime("%h"),
+        datetime.now().strftime("%d"), datetime.now().strftime("%H"),
+        datetime.now().strftime("%M"), datetime.now().strftime("%S"),
+        datetime.now().strftime("%f"))
 
     model_fn = "%s_Dz%d_Dy%d_Dx%d_bs%d_gss%d_gfs%d_dss%d_dfs%d_ts%d_r%d_eps%d_l%d_lr%d_nl%d_s%02d" % (
-        dname, z_dims_t, y_dims, Dx, batch_size, g_state_size, g_filter_size, d_state_size, d_filter_size,
-        time_steps, np.round(np.log10(reg_penalty)), np.round(np.log10(sinkhorn_eps)),
-        sinkhorn_l, np.round(np.log10(args.lr)), nlstm, seed)
+        dname, z_dims_t, y_dims, Dx, batch_size, g_state_size, g_filter_size,
+        d_state_size, d_filter_size, time_steps, np.round(np.log10(reg_penalty)), 
+        np.round(np.log10(sinkhorn_eps)), sinkhorn_l, np.round(np.log10(args.lr)), nlstm, seed)
 
     log_dir = "./trained/{}/log".format(saved_file)
 
@@ -187,14 +174,16 @@ def main():
 
             m_real_p = discriminator_m.call(real_data_p)
 
-            loss1 = gan_utils.compute_mixed_sinkhorn_loss(real_data, fake_data, m_real, m_fake, h_fake, scaling_coef,
-                                                          sinkhorn_eps, sinkhorn_l, real_data_p, fake_data_p, m_real_p,
-                                                          h_real_p, h_fake_p)
-            pm1 = gan_utils.scale_invariante_martingale_regularization(m_real, reg_penalty, scaling_coef)
+            loss1 = gan_utils.compute_mixed_sinkhorn_loss(
+                real_data, fake_data, m_real, m_fake, h_fake, scaling_coef,
+                sinkhorn_eps, sinkhorn_l, real_data_p, fake_data_p, m_real_p,
+                h_real_p, h_fake_p)
+            pm1 = gan_utils.scale_invariante_martingale_regularization(
+                m_real, reg_penalty, scaling_coef)
             disc_loss = - loss1 + pm1
         # update discriminator parameters
-        disch_grads, discm_grads = disc_tape.gradient(disc_loss, [discriminator_h.trainable_variables,
-                                                                  discriminator_m.trainable_variables])
+        disch_grads, discm_grads = disc_tape.gradient(
+            disc_loss, [discriminator_h.trainable_variables, discriminator_m.trainable_variables])
         dischm_optimiser.apply_gradients(zip(disch_grads, discriminator_h.trainable_variables))
         dischm_optimiser.apply_gradients(zip(discm_grads, discriminator_m.trainable_variables))
 
@@ -219,12 +208,14 @@ def main():
 
             m_real_p = discriminator_m.call(real_data_p)
 
-            loss2 = gan_utils.compute_mixed_sinkhorn_loss(real_data, fake_data, m_real, m_fake, h_fake, scaling_coef,
-                                                          sinkhorn_eps, sinkhorn_l, real_data_p, fake_data_p, m_real_p,
-                                                          h_real_p, h_fake_p)
+            loss2 = gan_utils.compute_mixed_sinkhorn_loss(
+                real_data, fake_data, m_real, m_fake, h_fake, scaling_coef,
+                sinkhorn_eps, sinkhorn_l, real_data_p, fake_data_p, m_real_p,
+                h_real_p, h_fake_p)
             gen_loss = loss2
         # update generator parameters
-        generator_grads = gen_tape.gradient(gen_loss, generator.trainable_variables)
+        generator_grads = gen_tape.gradient(
+            gen_loss, generator.trainable_variables)
         gen_optimiser.apply_gradients(zip(generator_grads, generator.trainable_variables))
         return loss2
 
@@ -275,4 +266,32 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='cot')
+    parser.add_argument('-d', '--dname', type=str, default='SineImage',
+                        choices=['SineImage', 'AROne', 'eeg'])
+    parser.add_argument('-t', '--test', type=str, default='cot',
+                        choices=['cot'])
+    parser.add_argument('-s', '--seed', type=int, default=42)
+    parser.add_argument('-gss', '--g_state_size', type=int, default=32)
+    parser.add_argument('-dss', '--d_state_size', type=int, default=32)
+    parser.add_argument('-gfs', '--g_filter_size', type=int, default=32)
+    parser.add_argument('-dfs', '--d_filter_size', type=int, default=32)
+    parser.add_argument('-r', '--reg_penalty', type=float, default=10.0)
+    parser.add_argument('-ts', '--time_steps', type=int, default=48)
+    parser.add_argument('-sinke', '--sinkhorn_eps', type=float, default=100)
+    parser.add_argument('-sinkl', '--sinkhorn_l', type=int, default=100)
+    parser.add_argument('-Dy', '--Dy', type=int, default=10)
+    parser.add_argument('-Dz', '--z_dims_t', type=int, default=10)
+    parser.add_argument('-g', '--gen', type=str, default="lstm",
+                        choices=["lstm", "fc"])
+    parser.add_argument('-bs', '--batch_size', type=int, default=32)
+    parser.add_argument('-nlstm', '--nlstm', type=int, default=1,
+                        help="number of lstms in discriminator")
+    parser.add_argument('-lr', '--lr', type=float, default=1e-3)
+    parser.add_argument('-bn', '--bn', type=int, default=1,
+                        help="batch norm")
+    
+    args = parser.parse_args()
+    
+    train(args)
+    
